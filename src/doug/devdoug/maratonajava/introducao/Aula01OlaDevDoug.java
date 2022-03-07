aeb6cdbfb0479b0a2b2ec3540dfadaf2115bb068
DevsJ <douglasmb87@gmail.com>
2022-03-07 13:00:44 -0300

    Aula 10

--- src/doug/devdoug/maratonajava/introducao/Aula01OlaDevDoug.java
+++ src/doug/devdoug/maratonajava/introducao/Aula01OlaDevDoug.java
@@ -3,7 +3,7 @@ package doug.devdoug.maratonajava.introducao;
 /**
  * isso e um comentario
  */
-public class OlaDevDoug {
+public class Aula01OlaDevDoug {
     public static void main(String[] args) {
           System.out.println("Ola Doug ");
     }
---
+++ src/doug/devdoug/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,13 @@
+package doug.devdoug.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    private static int ageIdade;
+
+    public static void main(String[] args) {
+        //int,double,float,char,byte,short,long,boolean
+        int ageIdade = 10;
+        System.out.println("a idade é "+ageIdade);
+
+
+    }
+}
