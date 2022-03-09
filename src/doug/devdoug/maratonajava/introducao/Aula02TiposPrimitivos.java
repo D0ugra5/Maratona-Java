020f5d35d8aa2907c4ccaf42752dbe924c1780ed
DevsJ <douglasmb87@gmail.com>
2022-03-09 15:42:32 -0300

    Aula 12 casting

--- src/doug/devdoug/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/doug/devdoug/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -5,7 +5,15 @@ public class Aula02TiposPrimitivos {
 
     public static void main(String[] args) {
         //int,double,float,char,byte,short,long,boolean
-        int ageIdade = 10;
+        int ageIdade = (int) 10000000000L;
+        long numeroGrande= 100000;
+        double salarioDouble = 2000;
+        float  salarioFloat = 2500.0f;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere  = 'M';
         System.out.println("a idade é "+ageIdade);
 
 
