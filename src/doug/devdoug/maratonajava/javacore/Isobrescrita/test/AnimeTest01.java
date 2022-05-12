ebf81214ba7bac388b8f6305628a44cd292e669d
DevsJ <douglasmb87@gmail.com>
2022-05-11 22:59:32 -0300

    Aula 78 Orientação a objetos

---
+++ src/doug/devdoug/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package doug.devdoug.maratonajava.javacore.Isobrescrita.test;
+
+import doug.devdoug.maratonajava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Rick Sanchez");
+        System.out.println(anime);
+    }
+}
---
+++ src/doug/devdoug/maratonajava/javacore/Jmodifiicadorfinal/test/CarroTest01.java
@@ -0,0 +1,11 @@
+package doug.devdoug.maratonajava.javacore.Jmodifiicadorfinal.test;
+
+import doug.devdoug.maratonajava.javacore.Jmodifiicadorfinal.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
